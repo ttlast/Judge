702a1363b7889854fd2ab00138ed82e013313332
SineTitans <565022817@qq.com>
2014-05-20 17:11:36 +0800

    okcall.h分为x86和x64两个版本

---
+++ judge/temp/Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	int a, b;
+	while (~scanf("%d%d", &a, &b))
+	{
+		printf("%d\n", a+b);
+	}
+	return 0;
+}
--- judge/test.sh
+++ judge/test.sh
@@ -2,6 +2,6 @@
 
 #make -B
 
-sudo ./Judge -l 2 -D /home/kidx/GzhuOJ/data/1044 -d temp -t 200 -m 65535 -o 81920
+sudo ./Judge -l 2 -D /home/user/GzhuOJ/data/1000 -d temp -t 200 -m 65535 -o 81920
 
 
